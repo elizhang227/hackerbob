fa21e2518aa6f179b8c495dbf015061472ad0de7
Eli Zhang <zhang.eli@hotmail.com>
2019-04-22 13:07:15 -0400

    added code to print any correct letter guesses

--- src/hackerBob.py
+++ src/hackerBob.py
@@ -3,6 +3,7 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 password = 'abc'
 ciphered = ""
 rotate_by = 13
+i = 0
 
 for char in user_input:
     index_in_alphabet = alphabet.index(char)
@@ -12,11 +13,17 @@ for char in user_input:
     ciphered += alphabet[new_index]
     #print(ciphered) #can comment out
 
-
 if ciphered == password:
     print('You got it right')
 else:
     print('Wrong sucka')
 
+### To return which letter was right ###
 
-
+# for char in ciphered:
+#     if ciphered == password[i]:
+#         print('you guessed it')
+#         i += 1
+#     else:
+#         print('you wrong')
+#         i += 1
